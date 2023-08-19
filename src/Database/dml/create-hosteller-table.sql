42b31e701a459a000d59e800469b8a0ba3ae85eb
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-08-19 15:39:35 +0530

    Initial Configuration

---
+++ src/Database/dml/create-hosteller-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS hosteller
+(
+    hosteller_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    gender CHAR(1),
+    date_of_birth DATE,
+    contact_no CHAR(10),
+    email VARCHAR(50)
+);
---
+++ src/env_example.php
@@ -0,0 +1,5 @@
+<?php
+define('SERVER', 'your_server_name');
+define('USERNAME', 'your_user_name');
+define('PASSWORD', 'your_password');
+define('DATABASE', 'your_database_name');
