be4ea6e25d4301f5092c00e73eafd7b101dc2dc9
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-08-22 15:38:11 +0530

    Created layout template

---
+++ src/View/layout.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hostel Management System - [Layout]</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row" style="background-color: #33A9FF;">
+            <?php require 'topnav.php'; ?>
+        </div>
+        <div class="row">
+            <aside class="col-2" style="background-color: gray;">
+            <?php require 'sidenav.php'; ?>
+            </aside>
+            <main class="col">
+                <div class="row">
+                    <!-- Main content goes here! -->
+                    <h1>[Layout]</h1>
+                </div>
+                <footer class="row" style="background-color:#33A9FF;">
+                <?php require 'footer.php'; ?>
+                </footer>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
