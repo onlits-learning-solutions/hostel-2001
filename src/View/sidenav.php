15bdcb95f38559ef11178c9b8f4595b574032563
mrityunjay9693 <mkaykumar123@gmail.com>
2023-08-26 14:45:20 +0530

    Create a link for dashboard in sidenav. Fixes #13

--- src/View/sidenav.php
+++ src/View/sidenav.php
@@ -1,4 +1,5 @@
 <nav style="padding-top: 10px; min-height:610px;">
+ <a href="layout.php?page=dashboard" style="display: block; text-decoration:none;">Dashboard</a>
     <a href="layout.php?page=hosteller" style="display: block; text-decoration:none;">Hosteller</a>
     <a href="layout.php?page=transaction" style="display: block; text-decoration:none;">Transaction</a>
 </nav>
